98260f907c77a58396e5a72e84ccbea14a06ef0f
GizemSaricicek <saricicek.gizem07@gmail.com>
2022-07-16 12:16:26 +0300

    spring projesi oluşturuldu.

---
+++ FilmProject/src/test/java/com/gizemsaricicek/filmproject/FilmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gizemsaricicek.filmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
