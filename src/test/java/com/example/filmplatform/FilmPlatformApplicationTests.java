36c9709082dca63129cb239c4f58a51329e5e32f
GizemSaricicek <saricicek.gizem07@gmail.com>
2022-07-16 23:16:29 +0300

    ..

---
+++ src/test/java/com/example/filmplatform/FilmPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
